404dd35ccc5592111a25187b4fd3c57f942ff21a
Jack Valmadre <jack.valmadre@gmail.com>
2022-05-10 21:03:55 +0930

    Add data to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y git
-apt-get install -y tmux
+apt-get install -y git tmux curl htop vim
+
+curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-384.0.1-linux-x86_64.tar.gz | tar -xz
+./google-cloud-sdk/install.sh
+gcloud init  # in new shell
+
+mkdir dl
+gsutil cp gs://aiml-valmadre-research-data/dl/inaturalist2021/* dl/
+
+mkdir data
+cd data
+ls ../dl/*.tar.gz | xargs -n 1 -P 2 tar -xvzf
 
 git clone https://gitlab.aiml.team/jack.valmadre/hier-class.git
-
+cd hier-class
 pip install --extra-index-url https://download.pytorch.org/whl/cu113 -r requirements.txt
